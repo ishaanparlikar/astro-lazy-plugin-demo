a94f6f239e67f9ecfdbe6049dd01be994d1beb92
--set <--set>
2026-01-14 00:48:09 +0530

    Initial Commit for site

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import { astroPrerenderIntegration } from 'vite-plugin-astro-prerender';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://ishaanparlikar.github.io',
+    base: '/vite-plugin-astro-prerender',
+    integrations: [
+        astroPrerenderIntegration({
+            componentsDir: 'src/components/Lazy',
+            outputDir: 'public/prerendered',
+            generateTailwindCSS: false,
+            renderer: "container"
+        }),
+    ],
+});
